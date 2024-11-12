f159484d789c20a5fa8df1f865fe255836c66147
Vladimir Skvortsov <mail@vladimirskvortsov.com>
2024-11-12 18:56:34 +0300

    Update

--- Skvortsov.Vladimir/lab2/src/conn.hpp
+++ Skvortsov.Vladimir/lab2/src/conn.hpp
@@ -7,7 +7,7 @@ class Conn {
 public:
   virtual ~Conn() {}
 
-  virtual bool read(std::string &data, size_t max_size) = 0;
+  virtual bool read(std::string& data, size_t max_size) = 0;
   virtual bool write(const std::string& data) = 0;
   virtual bool is_valid() const = 0;
 };
