9c107c5a68ffeab3603c932b0b4aa0fbc09902eb
Vladimir Skvortsov <mail@vladimirskvortsov.com>
2024-11-12 22:35:32 +0300

    Update

--- Skvortsov.Vladimir/lab2/src/conn_fifo.cpp
+++ Skvortsov.Vladimir/lab2/src/conn_fifo.cpp
@@ -8,7 +8,7 @@
 ConnFifo::ConnFifo(const std::string& path, bool create) : path(path), fd(-1), valid(false) {
   if (create) {
     // Create new FIFO
-    if (mkfifo(path.c_str(), 0644) == -1) {
+    if (mkfifo(path.c_str(), 0777) == -1) {
       if (errno != EEXIST) {
         std::cerr << "Ошибка создания FIFO: " << strerror(errno) << "\n";
         return;
@@ -16,13 +16,13 @@ ConnFifo::ConnFifo(const std::string& path, bool create) : path(path), fd(-1), v
     }
   }
 
-  fd = open(path.c_str(), create ? O_RDWR : O_RDONLY);
+  fd = open(path.c_str(), create ? O_RDONLY : O_RDWR);
   if (fd == -1) {
     std::cerr << "Error opening FIFO: " << strerror(errno) << "\n";
   } else {
     valid = true;
   }
-}
+};
 
 ConnFifo::~ConnFifo() {
   close();
@@ -32,44 +32,44 @@ ConnFifo::~ConnFifo() {
       std::cerr << "Error removing FIFO: " << strerror(errno) << "\n";
     }
   }
-}
+};
 
-bool ConnFifo::write(const std::string &msg) {
+bool ConnFifo::write(const std::string& msg) {
   if (!is_valid()) {
     std::cerr << "FIFO not available for writing\n";
     return false;
   }
 
-  ssize_t bytesWritten = ::write(fd, msg.c_str(), msg.size());
-  if (bytesWritten == -1) {
+  ssize_t bytes_written = ::write(fd, msg.c_str(), msg.size());
+  if (bytes_written == -1) {
     std::cerr << "Error writing to FIFO: " << strerror(errno) << "\n";
     return false;
   }
   return true;
 }
 
-bool ConnFifo::read(std::string &data, size_t max_size) {
+bool ConnFifo::read(std::string& data, size_t max_size) {
   if (!is_valid()) {
     std::cerr << "FIFO not available for reading\n";
     return false;
   }
 
   char buffer[max_size];
-  memset(buffer, 0, sizeof(buffer));
+  memset(buffer, '\0', max_size);
 
-  ssize_t bytesRead = ::read(fd, buffer, sizeof(buffer));
-  if (bytesRead == -1) {
+  ssize_t bytes_read = ::read(fd, buffer, max_size - 1);
+  if (bytes_read == -1) {
     std::cerr << "Error reading from FIFO: " << strerror(errno) << "\n";
     return false;
   }
 
-  data.assign(buffer, bytesRead);
+  data.assign(buffer, bytes_read);
   return true;
-}
+};
 
 bool ConnFifo::is_valid() const {
   return valid;
-}
+};
 
 void ConnFifo::close() {
   if (is_valid() && fd != -1) {
@@ -78,4 +78,4 @@ void ConnFifo::close() {
     }
     fd = -1;
   }
-}
+};
