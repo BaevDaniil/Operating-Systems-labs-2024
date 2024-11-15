4787e9e14adf7a55225b09008e43e2dd9469f8a1
theguydie <damir_asanov_03@mail.ru>
2024-11-15 17:30:26 +0300

    Start: 1) Add build.sh 2) main.cpp 3) folder laba2

---
+++ AsanovDamir/laba2/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p build
+cd build
+cmake ..
+make
+
+find . -type f -name '*.o' -delete
+find . -type f -name '*.cmake' -delete
+find . -type f -name 'CMakeCache.txt' -delete
+find . -type f -name 'Makefile' -delete
+rm -rf CMakeFiles
