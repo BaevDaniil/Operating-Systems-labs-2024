7d30da72cc78c5ef3902a752738c2a895a66831c
Max Goryunov <maximthehedgehog@yandex.ru>
2024-11-27 21:50:24 +0300

    NOT A REAL COMMIT; READ DESCRIPTION

--- Goryunov.Maxim/lab2/Conn.hpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-
-class Conn {
-public:
-    Conn() = default;
-    virtual ~Conn() = default;
-    virtual bool Read(std::string& message) = 0;
-    virtual bool Write(std::string const& message) = 0;
-}
